904fac37ce90fff564f299fb04057cf5b7ee4daa
huseyn1951 <sultanlkhuseyn@gmail.com>
2024-02-04 23:38:45 +0400

    sentence

---
+++ ConsoleApp24/SentenceExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    public static class SentenceExtension
+    {
+        public static void SentenceToSplit(this string sentence)
+        {
+            string[] split = sentence.Split(' ');
+
+            foreach (var item in split)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
